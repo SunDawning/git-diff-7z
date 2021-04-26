60ea9ffacbc40c935e075add8802c8d3855ea4c2
SunDawning <dpmeichen@gmail.com>
2021-04-26 13:10:56 +0800

    优化 部分按钮转换为输入框，并修改输入框和按钮的外观。

--- gui.html.js
+++ gui.html.js
@@ -2,6 +2,13 @@ import{postObjectInJSON}from"./postObjectInJSON.js";
 function gui(){
     let gui=document.createElement("div");
     document.body.appendChild(gui);
+    gui.style.cssText=`
+margin: 0 auto;
+width: 100%;
+max-width: 320px;
+text-align: center;
+padding: 8px;
+`;
     [
         {
             text:"选择Git仓库的目录",
@@ -37,17 +44,54 @@ function gui(){
             text:"打包",
             name:"gitDiff7z",
             onClick:function(event){
+                console.log("点击了打包按钮");
                 postObjectInJSON("/gitDiff7z",{
                     input:"c:/users/sgs/AppData/Roaming/literate-programming",
                     from:"6fba75e",
                 });
-            }
+            },
+            type:"button",
         }
     ].forEach(function(item){
-        let button=document.createElement("button");
-        gui.appendChild(button);
-        button.innerText=item.text;
-        button.addEventListener("click",item.onClick);
+        let type=item.type;
+        if(type==="button"){
+            let button=document.createElement("button");
+            button.type=item.type;
+            button.name=item.name;
+            gui.appendChild(button);
+            button.innerText=item.text;
+            button.addEventListener("click",item.onClick);
+            button.style.cssText=`
+    width: 304px;
+    font-size: 16px;
+    color: #fff;
+    border-width: 0;
+    box-shadow: none;
+    background: none;
+    outline: none;
+    height: 48px;
+    background-image: linear-gradient(129.12deg, #446dff 0%, rgba(99, 125, 255, 0.75) 100%);
+    border-radius: 10px;
+    backdrop-filter: blur(24px);
+    margin:8px;
+    cursor:pointer;
+`            
+        }else{
+            let input=document.createElement("input");
+            input.name=item.name;
+            input.placeholder=item.text;
+            gui.appendChild(input);
+            input.innerText=item.text;
+            input.style.cssText=`
+    margin:8px;
+    font-size: 14px;
+    background: rgba(39, 39, 41, 0.04);
+    border-radius: 8px;
+    padding-left: 12px;
+    width: 304px;
+    border: 1px solid transparent;
+    height: 46px;`
+        }
     });
 }
 export{gui}
